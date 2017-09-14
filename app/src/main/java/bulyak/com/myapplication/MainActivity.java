55c8303abe469d9d9008b2f618f5a56f9f4ffdf1
Henctv <1@bulyak.ru>
2017-09-14 20:31:00 +0500

    Добавил приветствие

--- app/src/main/java/bulyak/com/myapplication/MainActivity.java
+++ app/src/main/java/bulyak/com/myapplication/MainActivity.java
@@ -3,10 +3,14 @@ package bulyak.com.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +20,35 @@ public class MainActivity extends AppCompatActivity {
         Button button;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.greeting_layout);
-        button=(Button)findViewById(R.id.greeting_button);
+        button = (Button) findViewById(R.id.greeting_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Intent(MainActivity.this,AccountActivity.class);
+            }
+        });
 
-
-
-          }
-
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Privet Snack bar", Snackbar.LENGTH_SHORT)
+                        .setAction("Set Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this,
+                                        "Toast", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -39,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_settings:
 
             case R.id.account:
-                Intent intent=new Intent(MainActivity.this,AccountActivity.class);
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
                 break;
             default:
                 return super.onOptionsItemSelected(item);
